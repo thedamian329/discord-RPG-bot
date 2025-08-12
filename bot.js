d7afcebe88f42cfec556fe251931bc0adfc426c5
thedamian329 <thedamian329@hotmail.com>
2025-08-11 20:02:30 -0400

    33

--- bot.js
+++ bot.js
@@ -28,8 +28,6 @@ const scout = require("./commands/scout");
 const dice = require("./commands/dice");
 const skin = require("./commands/skin");
 
-//testing to see if this actually goes where its supposed to
-
 // Connect to the SQLite database
 let db = new sqlite3.Database("./rpg.db", (err) => {
   if (err) {
